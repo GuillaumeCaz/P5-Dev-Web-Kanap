3f53a183957b31b7eaaea6d1d018fc135cf83268
GuillaumeCaz <guillaume.cazeaudumec@gmail.com>
2022-12-30 18:21:40 +0100

    début de la partie pour la modif URL

--- front/html/index.js
+++ front/html/index.js
@@ -7,19 +7,28 @@
     
   })
   .then( function(value) {
+    console.table(value);
     for (let i=0 ; i < value.length; i++){
         let objet = value[i];
         document.getElementById('items').innerHTML += 
         `
-      <a>
-        <article>
+      <a href="product.html?id=${objet._id}" >
+        <article class="${i}">
           <img src="${objet.imageUrl}" alt="${objet.altTxt}">
           <h3 class="productName">${objet.name}</h3>
           <p class="productDescription">${objet.description}</p>
         </article>
       </a>`
+    }
+    const lien = document.querySelector("article");
+    lien.addEventListener('click', () =>{
+      console.log(lien);
+      let params = new URL(document.location).searchParams;
+      let id =params.get("id");
+    console.log(id);
+    })
     
-  }})
+  })
   .catch(function(err) {
     // Une erreur est survenue
   });
